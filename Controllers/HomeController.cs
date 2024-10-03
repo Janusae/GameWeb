23d5bfd949f23ec8bf563b6ff6f17d2fa71e0bba
Mohammad <me7445950@gmail.com>
2024-10-03 17:16:58 +0330

    Initial Project And Create two simple pages

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyTestProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(ILogger<HomeController> logger) 
+        {
+            _logger = logger; 
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult OurShop()
+        {
+            return View();
+        }
+    }
+}

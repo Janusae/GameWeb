b795431a714e5fd8eebcc31b95ae5e63df0350fb
Mohammad <me7445950@gmail.com>
2024-10-07 10:13:32 +0330

    Create two pages for Log in and Sign in (Just fron)

---
+++ Controllers/MembersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyTestProject.Controllers
+{
+    public class MembersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
